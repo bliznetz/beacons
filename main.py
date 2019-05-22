f631f80bae1aa4744e72d6dc500b70f798427909
Oksana Zhuravleva <Oksana_Zhuravleva@epam.com>
2019-05-23 02:01:15 +0300

    Added subprocess.check_output()

--- main.py
+++ main.py
@@ -2,34 +2,16 @@ import re
 import socket
 import datetime
 import subprocess
-output = subprocess.check_output(["python", "testblescan.py"]).decode("utf-8")
-
-testblescan='e1:dc:ab:d3:93:01,0112233445566778899aabbccddeeff0,10011,10003,-59,-62'
-topic = ((re.match('^([^,]+)', testblescan)).group()).replace(':', '') #Find first element befoe comma
-
-#print(topic)
+#output = subprocess.check_output(["python", "testblescan.py"], text=True)
+testblescan = subprocess.check_output(['cat','data.txt'], text=True)
 
 x=''
-for i in (testblescan.split(','))[2:6]: #Split string by comma, select 3,4,5 and 6 fields and insert in into a new string.
-    x = x + i + ' '
-#print(x)
-
-
+for line in testblescan.split('\n'): #Split string by comma, select 3,4,5 and 6 fields and insert in into a new string.
+    for t in (line.split(','))[2: 6]:
+        x = x + t + ' '
+    topic = ((re.match('^([^,]+)', line)).group()).replace(':', '')  # Find first element before comma
+    print(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x)
+    x=''
 #print(socket.gethostname())
 #print(datetime.datetime.now().strftime("%s"))
 
-
-print(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x)
-
-'''
-for i in first_par:
-    #print(i)
-    #print(type(i))
-    if i == (re.search(':', first_par)).group(0):
-        continue
-    else:
-        topic = topic + i
-        #print(i)
-        #print(topic)
-print(topic)
-'''
---
+++ test.py
@@ -0,0 +1,3 @@
+d = open('data.txt', 'r').read()
+#print(d.replace('\n',''))
+print(open('data.txt', 'r').read())
