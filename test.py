2edcd063f94347dc202ec7d691d5fbeaa7aefa98
Mira Coro <mirarat@gmail.com>
2019-05-23 19:47:37 +0300

    Finished parsing input data - from file as, from testblescan.py for prod. All works well.

--- test.py
+++ test.py
@@ -2,16 +2,13 @@ import re
 import socket
 import datetime
 import subprocess
-#Option text=True was added in Python 3.7. Raspberian has Python 3.5 and it's not straightforward to upgrade it
-testblescan = subprocess.Popen(['cat','data.txt'], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
-#testblescan = subprocess.Popen(['python', 'testblescan.py'], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+#output = subprocess.check_output(["python", "testblescan.py"], text=True)
+testblescan = subprocess.check_output(['cat','data.txt'], universal_newlines=True) #Option text=True was added in Python 3.7. Raspberian has Python 3.5 and it's not straightforward to upgrade it
 
 x=''
-for line in iter(testblescan.stdout.readline,''):
+for line in testblescan.split('\n'):   #Split one string into multipule
     for t in (line.split(','))[2: 6]:  #Split string by comma, select 3,4,5 and 6 fields and insert in into a new string.
-        x = x + ' ' + t
+        x = x + t + ' '
     topic = ((re.match('^([^,]+)', line)).group()).replace(':', '')  # Find first element before comma
-
-
-    print(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x, end='')
+    print(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x)
     x=''
