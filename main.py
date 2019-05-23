da30f00b8169ad0a991b64558ec2a8f9e54aa520
Mira Coro <mirarat@gmail.com>
2019-05-23 15:27:02 +0300

    Aded comments

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import subprocess
 testblescan = subprocess.check_output(['cat','data.txt'], text=True)
 
 x=''
-for line in testblescan.split('\n'): #Split string by comma, select 3,4,5 and 6 fields and insert in into a new string.
-    for t in (line.split(','))[2: 6]:
+for line in testblescan.split('\n'):   #Split one string into multipule
+    for t in (line.split(','))[2: 6]:  #Split string by comma, select 3,4,5 and 6 fields and insert in into a new string.
         x = x + t + ' '
     topic = ((re.match('^([^,]+)', line)).group()).replace(':', '')  # Find first element before comma
     print(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x)
