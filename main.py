21f872d5c71ebca998c6aead4f2e31d2e50eb94e
Mira Coro <mirarat@gmail.com>
2019-05-23 19:51:34 +0300

    Finished parsing input data - from file as, from testblescan.py for prod. All works well.

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import re
 import socket
 import datetime
 import subprocess
-#Option text=True was added in Python 3.7. Raspberian has Python 3.5 and it's not straightforward to upgrade it
+#Option 'text=True' was added in Python 3.7. Raspberian has Python 3.5 and it's not straightforward to upgrade it
 testblescan = subprocess.Popen(['cat','data.txt'], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
 #testblescan = subprocess.Popen(['python', 'testblescan.py'], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
 
 x=''
 for line in iter(testblescan.stdout.readline,''):
-    for t in (line.split(','))[2: 6]:  #Split string by comma, select 3,4,5 and 6 fields and insert in into a new string.
+    for t in (line.split(','))[2: 6]:  #Split string by comma, select 3, 4, 5 and 6 fields and insert it into a new string.
         x = x + ' ' + t
     topic = ((re.match('^([^,]+)', line)).group()).replace(':', '')  # Find first element before comma
 
