2c8c037480504cc12072ba7a181744650edcd4e7
Miroslav Andrushchenko <miroslav_andrushchenko@epam.com>
2019-07-22 16:33:25 +0300

    Fixed name of imported module

--- testblescan3.py
+++ testblescan3.py
@@ -16,7 +16,7 @@ blescan3.hci_le_set_scan_parameters(sock)
 blescan3.hci_enable_le_scan(sock)
 
 while True:
-	returnedList = blescan.parse_events(sock, 5)
+	returnedList = blescan3.parse_events(sock, 5)
 #	print "----------"
 	for beacon in returnedList:
 		print(beacon)
