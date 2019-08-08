a6ee44770f400b7dec5bc49d05a88f6b5c22b1c4
Oksana Zhuravleva <Oksana_Zhuravleva@epam.com>
2019-08-08 18:43:53 +0300

    Merge branch 'master' of /mnt/media/Python/Project/PyBlescan-CpToKafka with conflicts.

--- py3_test.py
+++ py3_test.py
@@ -3,13 +3,19 @@ import socket
 import datetime
 import subprocess
 import sys
+import sys
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 #import kafka
+#from kafka import KafkaProducer
+#from kafka.errors import KafkaError
 import logging
 import binascii
 import testblescan3
 
+#Option 'text=True' was added in Python 3.7. Raspberian has Python 3.5 and it's not straightforward to upgrade it
+testblescan = subprocess.Popen(['cat','data.txt'], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)  #For test purpose
+#testblescan = subprocess.Popen(['python', 'testblescan.py'], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
 
 
 def blescan_to_kafka(script):
@@ -41,7 +47,25 @@ def blescan_to_kafka(script):
 '''
 blescan_to_kafka ('testblescan3.py')
 # '10.66.216.17:9092')
+logger=logging.getLogger()
+x=''
+#producer = KafkaProducer(bootstrap_servers=['10.66.216.17:9092'])
+for line in iter(testblescan.stdout.readline,''):
+    for t in (line.split(','))[2: 6]:  #Split string by comma, select 3, 4, 5 and 6 fields and insert it into a new string.
+        x = x + ' ' + t
+    topic = ((re.match('^([^,]+)', line)).group()).replace(':', '')  # Find first element before comma
+    #try:
+    #    producer.send(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x, end='')
+    #except KafkaError:
+    #    logger.info("Can't send data to Kafka")
+
+    print(topic, socket.gethostname(), datetime.datetime.now().strftime("%s"), x, end='')  #
+
+    x=''
 
 #pip3 install kafka-python
 #pip3 install pybluez
 #pip3 install bitstring
+#pip3 install kafka-python
+
+
