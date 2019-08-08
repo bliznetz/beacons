ecf76f79e481ff741d5138f707171a13c4ba84bb
Oksana Zhuravleva <Oksana_Zhuravleva@epam.com>
2019-08-08 19:22:38 +0300

    Added file for test

--- test_testblescan3.py
+++ test_testblescan3.py
@@ -3,18 +3,22 @@ import socket
 import datetime
 import subprocess
 import sys
-from kafka import KafkaProducer
-from kafka.errors import KafkaError
+#from kafka import KafkaProducer
+#from kafka.errors import KafkaError
 #import kafka
+#from kafka import KafkaProducer
+#from kafka.errors import KafkaError
 import logging
 import binascii
-import testblescan3
+#import testblescan3
 
+#testblescan = subprocess.Popen(['python', 'testblescan.py'], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
 
 
 def blescan_to_kafka(script):
-
-    testblescan = subprocess.Popen(['python3', script], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+    # Option 'text=True' was added in Python 3.7. Raspberian has Python 3.5 and it's not straightforward to upgrade it
+    testblescan = subprocess.Popen(['cat', script], stdout=subprocess.PIPE, bufsize=1,universal_newlines=True)  # For test purpose
+    #testblescan = subprocess.Popen(['python3', script], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
     #logger=logging.getLogger()
     #producer = KafkaProducer(bootstrap_servers=[kafka_server], value_serializer=lambda v: v.encode('utf-8'))
     x=''
@@ -22,11 +26,12 @@ def blescan_to_kafka(script):
         for t in (line.split(','))[2: 6]:  #Split string by comma, select 3,4,5 and 6 fields and insert in into a new string.
             x = x + ' ' + t
         topic = ((re.match('^([^,]+)', line)).group()).replace(':', '')  # Find first element before comma
+        print(topic, socket.gethostname(), datetime.datetime.now().strftime("%s"), x, end='')  # For Python3
         x=''
-#        print(topic)
-    #sys.stdout.write('')
-    #print topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x, #For Python2
-        print(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x, end='') #For Python3
+
+        #sys.stdout.write('')
+        #print topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x, #For Python2
+
     '''
         try:
        # producer.send(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x)
@@ -39,9 +44,11 @@ def blescan_to_kafka(script):
             print('Can\'t send to Kafka')
         x=''
 '''
-blescan_to_kafka ('testblescan3.py')
+#blescan_to_kafka ('testblescan3.py')
+blescan_to_kafka ('data.txt')
 # '10.66.216.17:9092')
 
 #pip3 install kafka-python
 #pip3 install pybluez
 #pip3 install bitstring
+#pip3 install kafka-python
