8121f142293bb95ff4a9014cf9a5482166b268f8
Oksana Zhuravleva <Oksana_Zhuravleva@epam.com>
2019-08-09 17:58:33 +0300

    Have sent data without hostname and date

--- testblescan3_mod.py
+++ testblescan3_mod.py
@@ -4,6 +4,9 @@ import re
 import datetime
 import socket
 import bluetooth._bluetooth as bluez
+import logging
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
 
 dev_id = 0
 try:
@@ -17,6 +20,9 @@ except:
 blescan3.hci_le_set_scan_parameters(sock)
 blescan3.hci_enable_le_scan(sock)
 
+logger=logging.getLogger()
+producer = KafkaProducer(bootstrap_servers=['10.66.216.17:9092'], value_serializer=lambda v: v.encode('utf-8'))
+
 x=''
 while True:
 	returnedList = blescan3.parse_events(sock, 5)
@@ -27,9 +33,15 @@ while True:
             for t in (beacon.split(','))[2: 6]:  #Split string by comma, select 3,4,5 and 6 fields and insert in into a new string.
                 x = x + ' ' + t
             topic = ((re.match('^([^,]+)', beacon)).group()).replace(':', '')  # Find first element before comma
-#        print(topic)
-    #sys.stdout.write('')
-    #print topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x, #For Python2
-            print(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x, end='\n') #For Python3
+            #print(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x, end='\n') #For Python3
+            try:
+                #producer.send(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x)
+                producer.send(topic,x)
+                logger.info("Sent data to topic")
+                #print('{} sent succesfully to {}'.format(x, topic))
+                print("%s sent to %s" % (x, topic))
+            except KafkaError:
+                logger.info("Can't send data to Kafka")
+                print('Can\'t send to Kafka')
             x=''
             sys.stdout.flush()
