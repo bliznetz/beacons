b288d8c5db8c37ca34cb4d00832d00cdac2e6dfd
Oksana Zhuravleva <Oksana_Zhuravleva@epam.com>
2019-09-06 18:46:21 +0300

    Creation custom logger failed, just expanded simple logger's output

--- testblescan3.py
+++ testblescan3.py
@@ -11,25 +11,32 @@ import time
 kafka_server = '10.66.216.17:9092'
 dev_id = 0
 
-#logging.basicConfig(filename="/var/log/testblescan3.log", level=logging.INFO)
-logger = logging.getLogger('testblescan3')
-file_handler = logging.FileHandler('testblescan3.log')
+logging.basicConfig(filename="/var/log/testblescan3.log", level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%b-%d-%y %H:%M:%S')
+'''
+logger = logging.getLogger('iscan3')
+logger.handlers = []
+file_handler = logging.FileHandler('/iscan3/PyBlescan-CpToKafka/iscan3.log')
 file_handler.setLevel(logging.INFO)
 f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%b-%d-%y %H:%M:%S')
 file_handler.setFormatter(f_format)
 logger.addHandler(file_handler)
-logger.info("Program started")
+#logger.info("Program started")
+#logger.parent
+#logger.name
+'''
 
 try:
         sock = bluez.hci_open_dev(dev_id)
 #	print("ble thread started")
         #logger.info("Found Beacon MAC %s with UDID %s" % (blescan3.myFullList[0], blescan3.myFullList[1]))
-        logger.info("Found Beacon")
+        #logger.info("Found Beacon")
+        logging.info("Found Beacon")
 
 except:
 	#print("error accessing bluetooth device...")
 	#logger.exception("Error ocured while accessing bluetooth device MAC %s" %(blescan3.myFullList[0]))
-	logger.exception("Error ocured while accessing bluetooth device")
+	#logger.exception("Error ocured while accessing bluetooth device")
+	logging.exception("Error ocured while accessing bluetooth device")
 	sys.exit(1)
 
 blescan3.hci_le_set_scan_parameters(sock)
@@ -52,10 +59,12 @@ while True:
             value = socket.gethostname() + ' ' + data_time + x 
             try:
                 producer.send(topic, value)
-                logger.info("Location and data %s from host %s was sent to topic %s" % (x, socket.gethostname(), topic))
+                #logger.info("Location and data %s from host %s was sent to topic %s" % (x, socket.gethostname(), topic))
+                logging.info("Location and data %s from host %s was sent to topic %s" % (x, socket.gethostname(), topic))
                 #print("Location and data %s from host %s was sent to topic %s" % (x, socket.gethostname(), topic))
             except KafkaError:
-                logger.exception("Can't send geodata %s to Kafka topic %s" % (x, topic))
+                #logger.exception("Can't send geodata %s to Kafka topic %s" % (x, topic))
+                logging.exception("Can't send geodata %s to Kafka topic %s" % (x, topic))
                 #print('Can\'t send to Kafka')
             x=''
             sys.stdout.flush()
