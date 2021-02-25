c8c39366cfdc04e2aca97d787a87a0bf3b579c45
Kirill Kupriyanov <elephantenlein@gmail.com>
2021-02-25 15:45:21 +0300

    Added new Kafka server. Two kafka again.

--- testblescan3.py
+++ testblescan3.py
@@ -8,7 +8,7 @@ import logging
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 import time
-#kafka_server = '10.66.216.17:9092'
+kafka_server_new = '10.66.220.252:9092'
 kafka_server = '13.69.135.70:9092'
 dev_id = 0
 
@@ -28,15 +28,11 @@ logger.addHandler(file_handler)
 
 try:
         sock = bluez.hci_open_dev(dev_id)
-#	print("ble thread started")
         #logger.info("Found Beacon MAC %s with UDID %s" % (blescan3.myFullList[0], blescan3.myFullList[1]))
-        #logger.info("Found Beacon")
         logging.info("Found Beacon")
 
 except:
-	#print("error accessing bluetooth device...")
 	#logger.exception("Error ocured while accessing bluetooth device MAC %s" %(blescan3.myFullList[0]))
-	#logger.exception("Error ocured while accessing bluetooth device")
 	logging.exception("Error ocured while accessing bluetooth device")
 	sys.exit(1)
 
@@ -44,28 +40,24 @@ blescan3.hci_le_set_scan_parameters(sock)
 blescan3.hci_enable_le_scan(sock)
 
 producer = KafkaProducer(bootstrap_servers=[kafka_server], value_serializer=lambda v: v.encode('utf-8'))
+producer_new = KafkaProducer(bootstrap_servers=[kafka_server_new], value_serializer=lambda v: v.encode('utf-8'))
 
 x=''
 
 while True:
 	returnedList = blescan3.parse_events(sock, 5)
 	for beacon in returnedList:
-            #print(beacon)
 		
             for t in (beacon.split(','))[2: 9]:  #Split string by comma, select 3,4,5 and 6 fields and insert in into a new string.
                 x = x + ' ' + t
             topic = ((re.match('^([^,]+)', beacon)).group()).replace(':', '')  # Find first element before comma
-            #print(topic,socket.gethostname(),datetime.datetime.now().strftime("%s"),x, end='\n') #For Python3
             data_time = datetime.datetime.now().strftime("%s")
             value = socket.gethostname() + ' ' + data_time + x 
             try:
                 producer.send(topic, value)
-                #logger.info("Location and data %s from host %s was sent to topic %s" % (x, socket.gethostname(), topic))
+                producer_new.send(topic, value)
                 logging.info("Location and data %s from host %s was sent to topic %s" % (x, socket.gethostname(), topic))
-                #print("Location and data %s from host %s was sent to topic %s" % (x, socket.gethostname(), topic))
             except KafkaError:
-                #logger.exception("Can't send geodata %s to Kafka topic %s" % (x, topic))
                 logging.exception("Can't send geodata %s to Kafka topic %s" % (x, topic))
-                #print('Can\'t send to Kafka')
             x=''
             sys.stdout.flush()
